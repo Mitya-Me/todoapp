5c77fd70d380e58eee7101e79a4ccd4e308ee432
dmitry_mefodyev <dmitrymefodyev@gmail.com>
2021-10-14 14:16:39 +0300

    Add component Error

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Error = () => {
+    return (
+        <h2
+            style={{
+                fontSize: "35px",
+                textAlign: "center",
+                color: "dodgerblue",
+                padding: "25px",
+                lineHeight: "37px",
+            }}
+        >
+            Конгратулэйшнc, у вас не осталось больше дел.
+        </h2>
+    );
+};
+
+export default Error;
