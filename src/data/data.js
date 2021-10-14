27eabd5dbdc072df0fdf6ec7ca2fdc4d922c7f02
dmitry_mefodyev <dmitrymefodyev@gmail.com>
2021-10-14 14:17:26 +0300

    Add temporary data

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+let data = [
+    { id: 1, title: 'Buy bacteria', description: 'Buy two jars of bacteria to restore normal intestinal microflora' },
+    { id: 2, title: 'Pass vitamins to mom', description: 'Hand over the received vitamins to the customer' },
+    { id: 3, title: 'See a doctor on Tuesday', description: 'Go to the doctors office, report the test results, discuss the further course of treatment.' },
+    { id: 4, title: 'Buy olive oil', description: 'Olive oil will supply the right amount of vitamins to my body.' },
+]
+
+export default data;
